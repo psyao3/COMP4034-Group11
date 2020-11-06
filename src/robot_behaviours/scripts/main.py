20c36db85b556c816c3fe66f6a9d98ad15efad56
HungJ24 <Vngo24@yahoo.co.uk>
2020-11-06 16:25:05 +0000

    Update main.py

--- src/robot_behaviours/scripts/main.py
+++ src/robot_behaviours/scripts/main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-import rospy
-import tf
 import math
+import numpy as np
+import tf
 import random
-from std_msgs.msg import String
+import rospy
+
 from geometry_msgs.msg import Twist, Pose2D
-from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from math import cos, sqrt, pi
-import numpy as np
-from calculator import *
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String
 
 class robot_behaviours:
    
@@ -36,7 +36,7 @@ class robot_behaviours:
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
         # Define subscribers to listen to messages on /scan  and /odom topics 
-        rospy.Subscriber("/scan", LaserScan, self.scan_callback)
+        rospy.Subscriber('/scan', LaserScan, self.scan_callback)
         rospy.Subscriber('/odom', Odometry, self.odom_callback)
 
 
@@ -62,46 +62,43 @@ class robot_behaviours:
 
         # Decide on the behaviour to use
 
-        #### OBSTACLE AVOIDANCE
-
-             #### OBSTACLE AVOIDANCE
+        #### OBSTACLE AVOIDANCE ####
         
         # Intitialize empty list
         obst_dists = []
 
         # Get the average distance to obstacles around the turtlebot in specified ranges (in x degree segments)
-        # Observes a frontal cone in front of the turtlebot e.g (90 -> 0 -> 270)
-        # TODO: fix overlap between different ranges
-        for i in range(-45, 45, 8):
-            obst_dists.append(self.average_distance(ranges, i, 8))
+        # Observes a frontal cone in front of the turtlebot e.g (315 -> 0 -> 45) 
+        # +/- 50 from origin (0)
+        for i in range(-45, 45, 5):
+            obst_dists.append(self.average_distance(ranges, i, 5))
 
-        # Check obst_dists list to check if any are closer than 0.25
-        # Returns true if any values are <= 0.25, indicating a close obstacle.
-        #is_obstacle = any(x <= 0.25 for x in obst_dists)
-        #print(len(obst_dists))
-        #threshold = 0.25
-        right_obst = min(obst_dists[0:5])
-        front_obst = min(obst_dists[5:7])
-        left_obst = min(obst_dists[7:])
-
-        
+        # Check obst_dists list to check if any are closer than 0.5
+        # Returns true if any values are <= 0.5, indicating a close obstacle.
+      
+        right_obst = min(obst_dists[0:7])
+        front_obst = min(obst_dists[7:11])
+        left_obst = min(obst_dists[11:])
+     
         twist_msg = Twist()
+
+        #### RIGHT HAND WALL FOLLOWING ####
 
         # Note: positive angular speed is anticlockwise,
         #       negative is clockwise.
-        # Left side 
-        if left_obst <= 0.5:
-            rospy.loginfo("Obstacle left; Turning clockwise")
-            twist_msg.angular.z = -self.angular_speed
-            is_obstacle = True
-        # Right side
-        elif right_obst <= 0.5:
-            rospy.loginfo("Obstacle right; Turning anticlockwise")
+        # Right side 
+        if right_obst <= 0.5:
+            rospy.loginfo("Obstacle right less than 0.5m; Turning anticlockwise")
             twist_msg.angular.z = self.angular_speed
+            is_obstacle = True
+        # Left side
+        elif left_obst <= 0.5:
+            rospy.loginfo("Obstacle left less than 0.5m; Turning clockwise")
+            twist_msg.angular.z = -self.angular_speed
             is_obstacle = True
         # Ahead
         elif front_obst <= 0.5:
-            rospy.loginfo("Obstacle ahead; Turning anticlockwise")
+            rospy.loginfo("Obstacle ahead less than 0.5m; Turning anticlockwise")
             twist_msg.angular.z = self.angular_speed
             is_obstacle = True
         else: 
@@ -110,26 +107,17 @@ class robot_behaviours:
             is_obstacle = False
 
 
-       #left_obst, right_obst, front_obst, front_left_obst, front_right_obst = self.get_obstacle_distances(ranges)
-     
-        #### RIGHT HAND WALL FOLLOWING
-
-       
-
-        #### GET TWIST MESSAGE
+        #### GET TWIST MESSAGE ####
         # May need to swap obstacle/wall behaviour
 
         if is_obstacle:
-
-            #twist_msg = Twist()
-
 
             self.in_progress = False
             # Use obstacle avoidance behaviour
            
         else:
             # Random walk
-            #rospy.loginfo("Behaviour: Random walk")
+            # Passes closest distance to determine speed
             twist_msg = self.random_walk(ranges, min(front_obst, left_obst, right_obst))
                
        
@@ -138,9 +126,6 @@ class robot_behaviours:
         # Sleep according to the rate
         self.rate.sleep()
         
-
-
-
 
     # Walk 3m, rotate a random angle, repeat.
     def random_walk(self, ranges, closest_obst):
@@ -155,27 +140,42 @@ class robot_behaviours:
                 self.random_angle = random.uniform(-math.pi, math.pi)
 
         # Issue stop command if there is a close obstacle
-        if(closest_obst <= 0.25):
+        if(closest_obst <= 0.5):
             self.in_progress = False
             return Twist()
 
         # Move 3m
-        if calculate_linear_distance(self.initial_pose, self.pose) < 3:
-            rospy.loginfo("Walked %3fm out of 3m" % calculate_linear_distance(self.initial_pose, self.pose))
+        if self.calculate_linear_distance(self.initial_pose, self.pose) < 3:
+            rospy.loginfo("Walked %3fm out of 3m" % self.calculate_linear_distance(self.initial_pose, self.pose))
 
-            #front_obst = np.mean(ranges[range(-20, 20)])
-
-            # Need to set the speed proportional to the distance ahead - slow it down if approaching an object            
+            # Need to set the speed proportional to the distance ahead - slow it down if approaching an object
+            # Variable speed to account for drift/overshoot            
             twist_msg.linear.x = min(self.linear_speed * closest_obst, self.linear_speed)
         # Turn a random angle
-        elif calculate_angular_distance(self.initial_pose, self.pose) < self.random_angle:
-            rospy.loginfo("Turned %3f radians out of %3f" % (calculate_angular_distance(self.initial_pose, self.pose), self.random_angle))
+        elif self.calculate_angular_distance(self.initial_pose, self.pose) < self.random_angle:
+            rospy.loginfo("Turned %3f radians out of %3f" % (self.calculate_angular_distance(self.initial_pose, self.pose), self.random_angle))
             twist_msg.angular.z = self.angular_speed
         # Reset
         else:
             self.in_progress = False
 
         return twist_msg
+
+
+    def calculate_linear_distance(self, pose1, pose2):
+        x1, y1 = pose1.x, pose1.y
+        x2, y2 = pose2.x, pose2.y
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    def calculate_angular_distance(self, pose1, pose2):
+        theta1, theta2 = pose1.theta, pose2.theta
+
+        phi = abs(theta2 - theta1) % (2*math.pi)
+        if phi > math.pi:
+            distance = (2 * math.pi) - phi
+        else:
+            distance = phi
+        return distance
 
 
     # Calculate the average distance around the index given, to specified degree
