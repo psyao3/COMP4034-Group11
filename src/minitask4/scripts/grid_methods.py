17db26241295aa213a60f7039e0dfd89fd043d67
alex <alex-h@live.co.uk>
2020-11-17 18:18:07 +0000

    Updated grid_methods, added grid code to an odom_callback in minitask4_alt.py, not sure if it does what we need but it doesn't crash (on my pc at least).

--- src/minitask4/scripts/grid_methods.py
+++ src/minitask4/scripts/grid_methods.py
@@ -1,26 +1,38 @@
 import operator
 
 # Grid map variables
-resolution = 1 # Size of each grid cell.
-origin = [0, 0, 0] # [x, y, 0] defining the bottom left corner/origin of the grid. 
+resolution = 1.0 # Size of each grid cell.
+#origin = [0, 0, 0] # [x, y, 0] defining the bottom left corner/origin of the grid. 
 size = [20, 20] # No. of grid cells in [x, y] dimensions.
 
+origin = [-10.0, -10.0]
 # For mymap.yaml
 #resolution: 0.050000
 #origin: [-10.000000, -10.000000, 0.000000]
+
+# TODO: add conditionals for size (so its not exceeded), deal with
+# invalid values.
 
 def to_grid(p_xy, origin_, size, resolution):
     # Convert a world co-ordinate [px, py] with respect to the origin,
     # grid size and resolution, to [gx, gy], a pair of integer grid
     # co-ordinates
-    return [(x - y)/resolution for x, y in zip(p_xy, origin_)]
+
+    # Need to round or truncate real world co-ordinates - convert to Int
+
+    g_xy = [int((a - b)/resolution) for a, b in zip(p_xy, origin_)]
+    # If statements for exceeding 20x20 bounds?
+    return g_xy
 
 def to_world(g_xy, origin, size, resolution):
     # Return [px, py], float values representing the center of the grid cell in
     # world co-ordinates.
-    return 
+    
+    # Resolution needs to be a float here
+    p_xy = [(((a + b) * resolution) + resolution/2) for a, b in zip(g_xy, origin)]
+    return p_xy
 
-# Convert grid coordinate to map index
+# Convert grid coordinate to map index; size_x is expected to be size[0]
 def to_index(gx, gy, size_x):
   return gy * size_x + gx
     
@@ -29,6 +41,6 @@ def test():
     print to_grid( [5,5], [0,0],   [20,20], 1) #should return (5,5)
     print to_grid( [5,5], [0,0],   [20,20], 0.5) #should return (10,10)
     print to_grid( [0,0], [-5,-5], [20,20], 1) #should return (5,5)
-    print to_world( [9,9], [-5,-5], [20,20], 1) #should return (4.5, 4.5)
+    print to_world( [9,9], [-5,-5], [20,20], 1.0) #should return (4.5, 4.5)
 
 test()
--- src/minitask4/scripts/minitask4_alt.py
+++ src/minitask4/scripts/minitask4_alt.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist, Pose2D
+from geometry_msgs.msg import Twist, Pose2D, Point
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
-import cv2, cv_bridge
+from nav_msgs.msg import Odometry, OccupancyGrid
 import numpy as np
 import math
 import random
@@ -14,16 +12,19 @@ import tf
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from actionlib_msgs.msg import *
-from geometry_msgs.msg import Point
-import random
+
 import grid_methods as grid
 
 # note to self make sure the navigation file is running
 # roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=`rospack find minitask4`/maps/mymap.yaml
 
+
+
 class Waypoints:
 
     def __init__(self):
+
+        # TODO: add automatic 2d Pose estimate here?
 
         # Rooms 1 to 6. Found using rostopic echo /amcl_pose, and setting the 2d Pose
         # estimate in RViz to locations of interest (each room).
@@ -34,6 +35,13 @@ class Waypoints:
                       Point(-5.5, 4.78, 0),
                       Point(-5.74, -3.13, 0)]
 
+        # Listen to /odom and save xy position using a callback for the grid map.
+        self.pose = Pose2D()
+        self.sub = rospy.Subscriber('/odom', Odometry, self._odom_callback)
+
+        # Initialize occupancy grid; -1 means unknown/unvisited
+        # Length is size_x * size_y (variable imported from grid_methods)
+        self.occ_grid = np.full(np.product(grid.size), -1)
 
 
         while not rospy.is_shutdown():
@@ -83,7 +91,30 @@ class Waypoints:
             rospy.loginfo("The robot failed to reach the destination")
             
 
-       
+    def _odom_callback(self, msg):
+        # Get (x, y, theta) specification from odometry topic
+        quarternion = [msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,\
+                    msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(quarternion)
+
+        self.pose.theta = yaw
+        px = self.pose.x = msg.pose.pose.position.x
+        py = self.pose.y = msg.pose.pose.position.y
+
+        # Get grid position
+        g_xy = grid.to_grid([px, py], grid.origin, grid.size, grid.resolution)
+
+        # Set current grid position on occ_grid to visited
+        index = grid.to_index(g_xy[0], g_xy[1], grid.size[0])
+
+        # Debug:
+        #print g_xy
+        #print index
+
+        # 0 indicates a grid square has been visited.
+        self.occ_grid[index] = 0  
+
+        # TODO: Could work out proportion of grid visited and print updates. 
 
 
 
