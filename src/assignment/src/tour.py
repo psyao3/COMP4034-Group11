9b3de6b6e05d23f5ff8a177eb097d5311ae5ff83
alex <37447102+alexban94@users.noreply.github.com>
2020-12-04 19:36:19 +0000

    Add files via upload

--- src/assignment/src/tour.py
+++ src/assignment/src/tour.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from smach import StateMachine
-from smach_ros import SimpleActionState
+import Graph
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 from geometry_msgs.msg import Twist, Pose2D, Point
@@ -12,24 +11,68 @@ from callbacks import  *
 from actionlib_msgs.msg import *
 import actionlib
 
-waypoints = [
-    ["a", Point(-1.1, 3.8, 0)],
-    ["b", Point(-0.88, -3.63, 0)],
-    ["c", Point(-0.13, -0.73, 0)],
-    ["d", Point(0.19, 4.25, 0)],
-    ["e", Point(4.23, 3.96, 0)],
-    ["f", Point(3.93, 2.43, 0)],
-    ["g", Point(1.48, 2.38, 0)],
-    ["h", Point(0.9, -1.44, 0)],
-    ["i", Point(0.79, -3.77, 0)],
-    ["j", Point(3.48, -3.9, 0)],
-    ["k", Point(2.53, -1.42, 0)],
-    ["l", Point(2.89, 0.98, 0)],
-    ["m", Point(4.86, 0.85, 0)],
-    ["n", Point(5.85, 3.48, 0)],
-    ["o", Point(5.34, -2.33, 0)]
-]
+graph = { "a" : ["b"],
+          "b" : ["a","c"],
+          "c" : ["b","d"],
+          "d" : ["c", "e", "k"],
+          "e" : ["d", "f", "n"],
+          "f" : ["e", "g"],
+          "g" : ["f", "h", "j"],
+          "h" : ["g", "i"],
+          "i" : ["h", "j"],
+          "j" : ["g", "i"],
+          "k" : ["d", "l"],
+          "l" : ["k", "m"],
+          "m" : ["l", "n"],
+          "n" : ["m", "e", "o"],
+          "o" : ["n", "p", "q"],
+          "p" : ["o", "q"],
+          "q" : ["o", "p", "r"],
+          "r" : ["q"]
+        }
 
+node_locations = {
+          "a" : Point(-1.1, 3.8, 0),
+          "b" : Point(-1.1, 1.36, 0),
+          "c" : Point(-1.1, -1.22, 0),
+          "d" : Point(-0.01, -0.34, 0),
+          "e" : Point(0.07, 0.98, 0),
+          "f" : Point(0.18, 4.17, 0),
+          "g" : Point(1.62, 4.11, 0),
+          "h" : Point(4.14, 3.93, 0),
+          "i" : Point(3.82, 2.32, 0),
+          "j" : Point(1.45, 2.5, 0),
+          "k" : Point(0.77, -1.63, 0),
+          "l" : Point(0.612, -3.53, 0),
+          "m" : Point(3.07, -3.6, 0),
+          "n" : Point(2.66, 1.04, 0),
+          "o" : Point(4.65, 0.88, 0),
+          "p" : Point(5.77, 3.51, 0),
+          "q" : Point(5.77, 0.78, 0),
+          "r" : Point(5.77, -2.55, 0)
+}
+
+visited_nodes = {}
+unvisited_nodes = node_locations.copy()
+
+def patrol():
+    # Empty dictionaries {} evaluate to False
+    while not unvisited_nodes:
+        # Get next node/key
+        node = list(unvisited_nodes)[0]
+        target = unvisited_nodes.pop(node)
+
+        # Need to navigate to the target via defined edges
+
+# Take the start/end node and existing path. If this is the first
+# function call, then path = []
+def find_path(graph, start_node, end_node, path):
+    # Add the current start node to the path list.
+    path.append(start_node)
+
+    # Destination is reached, so the entire path is found
+    if start_node == end_node:
+        return path
 
 
 def move_to_target(coordinates):
